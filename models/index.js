3c8464e4c1189a80ab553da01397778ddd55c12c
Moo Point <beachgal0105@gmail.com>
2020-08-16 01:01:28 -0700

    crud files

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+
+module.exports = { User };
+
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
