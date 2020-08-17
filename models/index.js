a61ffce8814913042e89e33bb05ae8d98f07d24c
Moo Point <beachgal0105@gmail.com>
2020-08-16 22:50:16 -0700

    post updates

--- models/index.js
+++ models/index.js
@@ -7,6 +7,16 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+  
 router.use('/users', userRoutes);
 
 module.exports = router;
